247d3a3955d0c302be6304d3e905b6a4b68eda72
Edy Epumuceno Rodrigues Junior <edyjrdev@gmail.com>
2025-11-07 13:07:49 -0300

    feat solucao exercicio

---
+++ desafio_001.py
@@ -0,0 +1,22 @@
+"""
+Instruções:
+O programa deve começar solicitando ao usuário que insira seu nome.
+Em seguida, o programa deve pedir ao usuário para inserir o valor do seu salário. Considere que este valor pode ser um número decimal.
+Depois, o programa deve solicitar a porcentagem do bônus recebido pelo usuário, que também pode ser um número decimal.
+O cálculo do KPI do bônus de 2024 é de 1000 + salario * bônus
+Finalmente, o programa deve imprimir uma mensagem no seguinte formato: "Olá [nome], o seu valor bônus foi de 5000".
+Exemplo de Saída:
+Se o usuário digitar "Luciano" como nome, "5000" como salário e "1.5" como bônus, o programa deve imprimir:
+"""
+# Entradas
+nome = input('Digite seu nome: ')
+salario_str = input('Informe o valor do seu salário: ')
+bonus_str = input('Informe o valor percentual do seu bônus:')
+
+# processamento 
+salario = float(salario_str)
+bonus = float(bonus_str)
+kpi = 1000 + (salario * bonus)
+
+# saida
+print(f'Olá {nome}, o seu valor bônus foi de {kpi}')
---
+++ exerc_002.py
@@ -0,0 +1,8 @@
+# criar programa pra soma de 2 valores passados pelo usuario
+
+num = input('Digite o primeiro valor: ')
+num2 = input('Digite o segundo valor: ')
+
+soma = int(num) + int(num2)
+
+print("Soma é " + str(soma))
